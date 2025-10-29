cc75aa11fc95f04eba6505c46c7acee5770ffbe5
Skomaiya <s.komaiya@alustudent.com>
2025-10-29 19:16:30 +0200

    Create user role permissions

---
+++ backend/api/permissions.py
@@ -0,0 +1,34 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Custom permission to only allow admin users
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+
+class IsStudent(permissions.BasePermission):
+    """
+    Custom permission to only allow students to perform certain actions
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'student'
+
+
+class IsInstructor(permissions.BasePermission):
+    """
+    Custom permission to only allow instructors to perform certain actions
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.role == 'instructor'
+
+
+class IsCourseOwner(permissions.BasePermission):
+    """
+    Permission to only allow course owners (instructors) to modify their courses
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.instructor == request.user
